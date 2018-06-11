4832a812337541e23a0fc10de7c8f391357b00b9
Maxime Bento <32094911+Ekineox@users.noreply.github.com>
2018-06-12 00:52:09 +0200

    Update WaterAlarm.py

--- WaterAlarm.py
+++ WaterAlarm.py
@@ -8,12 +8,11 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 pin = 21
-
+waterAlarm = 0
 
 GPIO.setup(pin, GPIO.IN)
 
 while waterAlarm == 0: # Tant que i est strictement inférieure à 10
-
-  state = GPIO.input(pin)
-  print(state)
-  time.sleep(5)
+    state = GPIO.input(pin)
+    print(state)
+    time.sleep(5)
